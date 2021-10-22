b49f87fffbc50db6a71b1da9e572961e1a4bc82b
arnob-das <contact.arnobdas@gmail.com>
2021-10-23 01:49:22 +0600

    header implementation done

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     shadow-md bg-white"
     >
       <Button
-        color="blue"
+        color="gray"
         buttonType="outline"
         rounded={true}
         iconOnly={true}
@@ -21,12 +21,32 @@ function Header() {
       <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
 
       <div
-        className="flex flex-grow items-center px-5 py-2
-    bg-gray-100 text-gray-600 rounded-lg"
+        className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2
+    bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md"
       >
         <Icon name="search" size="3xl" color="gray" />
-        <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none"/>
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 text-base bg-transparent outline-none"
+        />
       </div>
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className="ml-5 md:ml-20 h-20 w-20 border-0"
+      >
+        <Icon name="apps" size="3xl" color="gray" />
+      </Button>
+
+      <img 
+      loading="lazy"
+      className="cursor-pointer h-12 w-12 rounded-full ml-2"
+      src="https://arnobdas.netlify.app/img/arnob_profilep.png"
+      />
     </div>
   );
 }
